02efe9a726aafd3f8815e1c53da5eed8b3daf37f
02pocket <02pocket@anymail.com>
2022-09-09 11:03:40 +0800

    add color picker function

--- lua/user/config/telescope-custom.lua
+++ lua/user/config/telescope-custom.lua
@@ -45,6 +45,45 @@ mappings.find_in_java_it_resource_dir = function()
     }
     require'telescope.builtin'.find_files(opts)
 end
-print('telescope-custom')
+
+local pickers = require'telescope.pickers'
+local finders = require'telescope.finders'
+local conf = require'telescope.config'.values
+
+local actions = require'telescope.actions'
+local action_state = require'telescope.actions.state'
+
+
+local colors = function(opts)
+    opts = opts or {}
+    pickers.new(opts,{
+        prompt_tile = 'colors',
+        finder = finders.new_table{
+            results = {
+                { "red", "#ff0000"},
+                { "green", "#00ff00" },
+                { "blue", "#0000ff" }
+            },
+            entry_maker = function(entry)
+                return {
+                    value = entry,
+                    display = entry[1],
+                    ordinal = entry[1]
+                }
+            end
+        },
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function ()
+               local selection = action_state.get_selected_entry()
+               print(vim.inspect(selection))
+               vim.api.nvim_put({ selection[1] },"",false,true)
+            end)
+            return true
+        end
+    }):find()
+end
+
+mappings.pick_colors = colors
 
 return mappings
