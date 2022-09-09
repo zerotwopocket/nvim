52f60e652f98311217794a7b201d6942af8379d4
02pocket <02pocket@anymail.com>
2022-09-09 11:03:03 +0800

    change hop mappings

--- lua/user/config/hop.lua
+++ lua/user/config/hop.lua
@@ -1,6 +1,6 @@
 return function()
     require'hop'.setup{}
     local opts = { noremap=true, silent = true }
-    vim.api.nvim_set_keymap('n','<leader>hw','<cmd>HopWord<cr>',opts)
-    vim.api.nvim_set_keymap('n','<leader>hc','<cmd>HopChar1<cr>',opts)
+    vim.api.nvim_set_keymap('n','<M-w>','<cmd>HopWord<cr>',opts)
+    vim.api.nvim_set_keymap('n','<M-c>','<cmd>HopChar1<cr>',opts)
 end
